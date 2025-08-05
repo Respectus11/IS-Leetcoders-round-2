85690ee01c247d5da3208032ab6d6d9aa5d5d950
blen gebre <blengebre2121@gmail.com>
2025-08-05 22:30:31 +0300

    Add solution for goal parser interpretation

---
+++ Problems/002-goal-parser-interpretation/blen.py
@@ -0,0 +1,15 @@
+
+class Solution:
+    def interpret(self, command: str) -> str:
+        BE = ""
+        i = 0
+        while i < len(command):
+            if command[i] == 'G':
+                BE += 'G'
+                i += 1
+            elif command[i] == '(' and command[i+1] == ')':
+                BE += 'o'
+                i += 2
+            elif command[i] == '(' and command[i+1:i+4] == 'al':
+                BE += 'al'
+        return B
