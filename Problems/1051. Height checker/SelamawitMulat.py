d2b2e965f3070b7c295a8494d7ff839eba1974d7
Selamawit  Mulat <selamawitmulat0@gmail.com>
2025-08-05 13:23:03 +0300

    Add so/n to Height checker

---
+++ Problems/1051. Height checker/SelamawitMulat.py
@@ -0,0 +1,7 @@
+def heightChecker(self,heights):
+    count=0
+    expected=sorted(heights)
+    for i in range(len(heights)):
+        if heights[i]!=expected[i]:
+            count+=1
+    return count
