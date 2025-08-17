b9e3276c0f06b5e7107a0e804de2b3e95e811fdb
Pilupadare <kalkidan.ugr-1018-17@aau.edu.et>
2025-08-17 03:50:58 -0700

    pilupadare.py

--- Problems/Rotate_string/pilupadare.py
+++ Problems/Rotate_string/pilupadare.py
@@ -1,1 +1,6 @@
- 
+ class Solution(object):
+    def rotateString(self, s, goal):
+        if(len(s)!=len(goal)): 
+            return False
+        s=s+s
+        return True if s.find(goal)!= -1 else False
